0277a7aa3d9658dbd6068cb4aa63a30528eea686
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-26 13:33:33 +0100

    feat: set target contract address

--- frontend/src/stacks/tx.ts
+++ frontend/src/stacks/tx.ts
@@ -1,3 +1,4 @@
 import { makeContractCall, broadcastTransaction, AnchorMode, PostConditionMode } from "@stacks/transactions";
 export async function invokePayDayExt() {
   const txOptions = {
+    contractAddress: "ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM",
