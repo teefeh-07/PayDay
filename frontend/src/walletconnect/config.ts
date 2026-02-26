7910fd2f83c96ec31cd7e34aefdf8ebf49fe45ba
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-26 13:34:01 +0100

    feat: create initialization function for walletconnect

--- frontend/src/walletconnect/config.ts
+++ frontend/src/walletconnect/config.ts
@@ -1,3 +1,4 @@
 import { Core } from "@walletconnect/core";
 import { Web3Wallet } from "@walletconnect/web3wallet";
 const core = new Core({ projectId: "YOUR_PROJECT_ID" });
+export async function initWalletConnect() {
