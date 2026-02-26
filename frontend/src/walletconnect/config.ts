321f1954d21b1902d562d8809f0af81466bf3511
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-26 13:34:02 +0100

    feat: invoke Web3Wallet init

--- frontend/src/walletconnect/config.ts
+++ frontend/src/walletconnect/config.ts
@@ -2,3 +2,4 @@ import { Core } from "@walletconnect/core";
 import { Web3Wallet } from "@walletconnect/web3wallet";
 const core = new Core({ projectId: "YOUR_PROJECT_ID" });
 export async function initWalletConnect() {
+  const web3wallet = await Web3Wallet.init({
