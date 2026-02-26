b0235322854b2e184eabf9484add067f616c860c
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-26 13:33:02 +0100

    feat: create user session instance

--- frontend/src/stacks/auth.ts
+++ frontend/src/stacks/auth.ts
@@ -1,2 +1,3 @@
 import { AppConfig, UserSession, showConnect } from "@stacks/connect";
 export const appConfig = new AppConfig(["store_write", "publish_data"]);
+export const userSession = new UserSession({ appConfig });
