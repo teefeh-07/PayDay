1433416372e0f84abd5368ff7add7c816c7d1795
Boluwatife Oniyilo <boluwatifeoniyilo@gmail.com>
2026-02-26 13:33:25 +0100

    Implementation: Feat Stacks Connect Setup (#3)
    * feat: add stacks connect to dependencies
    
    * feat: import stacks connect components
    
    * feat: initialize AppConfig for stacks
    
    * feat: create user session instance
    
    * feat: define authentication routine
    
    * feat: invoke showConnect dialog
    
    * feat: add app details for connect popup
    
    * feat: set redirect path to root
    
    * feat: handle successful connect login
    
    * feat: attach user session to connect options
    
    * feat: finalize connect options and auth function

---
+++ frontend/src/stacks/auth.ts
@@ -0,0 +1,11 @@
+import { AppConfig, UserSession, showConnect } from "@stacks/connect";
+export const appConfig = new AppConfig(["store_write", "publish_data"]);
+export const userSession = new UserSession({ appConfig });
+export function authenticate() {
+  showConnect({
+    appDetails: { name: "PayDay", icon: window.location.origin + "/icon.png" },
+    redirectTo: "/",
+    onFinish: () => { window.location.reload(); },
+    userSession,
+  });
+}
