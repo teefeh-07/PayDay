4bde283389800599e0240ea007124b40e1d8adee
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-27 17:02:09 +0100

    feat: add Default story for Dashboard

--- frontend/src/stories/Dashboard.stories.tsx
+++ frontend/src/stories/Dashboard.stories.tsx
@@ -11,3 +11,8 @@ const meta: Meta<typeof Dashboard> = {
 
 export default meta;
 type Story = StoryObj<typeof meta>;
+
+
+export const Default: Story = {
+  args: { title: 'Dashboard', loading: false },
+};
