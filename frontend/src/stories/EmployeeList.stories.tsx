17577605b28b9e9eff1e2ece6ee3d026cf9dfeca
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-27 17:02:22 +0100

    feat: define Storybook meta configuration for EmployeeList

--- frontend/src/stories/EmployeeList.stories.tsx
+++ frontend/src/stories/EmployeeList.stories.tsx
@@ -1,3 +1,13 @@
 // EmployeeList component stories
 import type { Meta, StoryObj } from '@storybook/react';
 import { EmployeeList } from '../components/EmployeeList';
+
+
+const meta: Meta<typeof EmployeeList> = {
+  title: 'PayDay/EmployeeList',
+  component: EmployeeList,
+  parameters: { layout: 'centered' },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
