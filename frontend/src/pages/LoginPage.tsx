c9b3221ed1bdb306197226bad1b5df24d0d1ad6e
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-27 16:46:14 +0100

    feat: scaffold LoginPage functional component

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -8,3 +8,7 @@ import { useNavigate } from 'react-router-dom';
 interface LoginPageProps {
   isAuthenticated?: boolean;
 }
+
+
+export const LoginPage: React.FC<LoginPageProps> = ({ isAuthenticated }) => {
+  const navigate = useNavigate();
