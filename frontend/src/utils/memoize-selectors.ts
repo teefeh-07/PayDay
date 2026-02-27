1729700c6d82c0408bf51c21603cf48b15c92719
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-27 17:03:26 +0100

    perf: implement memoize-selectors utility function

---
+++ frontend/src/utils/memoize-selectors.ts
@@ -0,0 +1,8 @@
+// Memoize expensive selectors with useMemo
+// Performance utility: memoize-selectors
+
+export function applyOptimization() {
+  console.log('memoize-selectors: optimization applied');
+}
+
+export default applyOptimization;
